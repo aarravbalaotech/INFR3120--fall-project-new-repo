3e7a3970732786fcd17b00e5e278f5d9286d511d
aarravbalaotech <aarrav.bala@ontariotechu.net>
2025-12-06 01:11:24 -0500

    Profile page creating and feature add ins

--- public/Script/app.js
+++ public/Script/app.js
@@ -1,8 +1,20 @@
-//IIFI
-// Immediately invoed function expression
-(function(){
-    function start(){
-        console.log("App started...")
-    }
-    window.addEventListener("load",start);
+
+// IIFE
+(function () {
+  function start() {
+    console.log("App started...");
+  }
+  window.addEventListener("load", start);
 })();
+
+function openProfileModal() {
+  const modal = document.getElementById("profileModal");
+  if (modal) modal.classList.remove("hidden");
+}
+
+function closeProfileModal() {
+  const modal = document.getElementById("profileModal");
+  if (modal) modal.classList.add("hidden");
+}
+
+  
